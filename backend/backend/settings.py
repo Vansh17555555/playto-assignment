3a06284c7f5ae94ce53dbd47bce2e5adba6efd0b
Vansh Verma <vvansh739@gmail.com>
2026-04-29 01:05:34 +0530

    cors fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,6 +76,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://playto-assignment-3.onrender.com",
 ]
 CORS_ALLOW_HEADERS = [
     "accept",
